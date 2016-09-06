31fc329911fb6b3ea2b2ee4428e15297193e014a
Guido <guidosergio95@hotmail.com>
2016-09-06 18:06:48 -0300

    recibirMensajes()

--- tp1/src/CodificadorDeMensajes.cpp
+++ tp1/src/CodificadorDeMensajes.cpp
@@ -1,6 +1,6 @@
 #include "CodificadorDeMensajes.h"
 
-CodificadorDeMensajes::CodificadorDeMensajes(SOCKET socketD){
+CodificadorDeMensajes::CodificadorDeMensajes(SOCKET socketD) {
 	con = new Conexion(socketD);
 }
 
@@ -8,7 +8,7 @@ CodificadorDeMensajes::~CodificadorDeMensajes() {
 	delete con;
 }
 
-void CodificadorDeMensajes::enviarMensajeFormateado(std::string destinatario, std::string texto){
+void CodificadorDeMensajes::enviarMensajeFormateado(std::string destinatario, std::string texto) {
 	con->enviar("SND_MESSAGE");
 	std::string resp = con->recibir();
 	if (resp.substr(0, resp.find('-')) == "1") {
@@ -34,5 +34,22 @@ void CodificadorDeMensajes::enviarMensajeFormateado(std::string destinatario, st
 }
 
 void CodificadorDeMensajes::recibirMensajes() {
+	con->enviar("RCV_MESSAGES");
+	std::string resp = con->recibir();
+	if (resp.substr(0, resp.find('-')) == "1") {
+		int cantidadDeMensajes = std::stoi(resp.substr(resp.find('-1'), 1));
+		while (cantidadDeMensajes > 0) {
+			con->enviar("DOWNLOAD_MESSAGES");
+			resp = con->recibir();
+			if (resp.substr(0, resp.find('-')) == "1") {
+				resp = resp.substr(resp.find('-'));
+				parsearMensaje(resp);
+				cantidadDeMensajes--;
+			}
+		}
+	}
+}
+
+void CodificadorDeMensajes::parsearMensaje(std::string mensaje) {
 
 }
