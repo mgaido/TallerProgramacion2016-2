4304f60f8ba67777a680fe00073298d044235797
mrodac <github@mrod.com.ar>
2016-09-07 18:38:32 -0300

    Un poco de logging

--- tp1/src/Servidor.cpp
+++ tp1/src/Servidor.cpp
@@ -1,24 +1,29 @@
 #include "Servidor.h"
 
-Servidor::Servidor(std::string &archivo) {
-	socketD = INVALID_SOCKET;
+Servidor::Servidor(int puerto) {
+	this->puerto = puerto;
 	detenido = false;
-	Usuarios::leerUsuarios(archivo);
+	socketD = INVALID_SOCKET;
 }
 
 Servidor::~Servidor() {
 	detener();
 }
 
-void Servidor::iniciar(int port) {
-	thread = std::thread(&Servidor::aceptarConexiones, this, port);
-	std::cout << "Presiona enter para terminar..." << std::endl;
-	std::string msg;
-	std::getline(std::cin, msg);
+void Servidor::iniciar() {
+	int response = crearSocket();
+	if (response < 0) {
+		std::cerr << "No se pudo iniciar la conexión" << std::endl;
+	} else {
+		thread = std::thread(&Servidor::aceptarConexiones, this);
+		std::cout << "Presiona enter para terminar..." << std::endl;
+		std::string msg;
+		std::getline(std::cin, msg);
+	}
 	detener();
 }
 
-void Servidor::aceptarConexiones(int port) {
+int Servidor::crearSocket() {
 	int response = -1;
 
 	std::string funcion = "socket";
@@ -30,65 +35,74 @@ void Servidor::aceptarConexiones(int port) {
 		struct sockaddr_in serverAddress;
 		serverAddress.sin_family = AF_INET;
 		serverAddress.sin_addr.s_addr = INADDR_ANY;
-		serverAddress.sin_port = htons(port);
+		serverAddress.sin_port = htons(puerto);
 
 		funcion = "bind";
-		response = bind(socketD, (struct sockaddr*) &serverAddress, sizeof(serverAddress));
+		response = bind(socketD, (struct sockaddr*) &serverAddress,
+				sizeof(serverAddress));
 		if (response == 0) {
 			funcion = "listen";
 			response = listen(socketD, 4);
-			if (response == 0) {
-				struct sockaddr_in clientAddress;
-				int clientAddressLength = sizeof(clientAddress);
-				char ip[INET_ADDRSTRLEN];
-
-				funcion = "accept";
-				if (response == 0) {
-					while (!detenido) {
-						SOCKET newSocketD = accept(socketD, (struct sockaddr*) &clientAddress,
-								(socklen_t*)&clientAddressLength);
-
-						if (newSocketD != INVALID_SOCKET) {
-							inet_ntop(AF_INET, &(clientAddress.sin_addr), ip, INET_ADDRSTRLEN);
-							sesiones.push_back(Sesion(newSocketD, ip));
-						} else if (! detenido)
-							response = -1;
-
-						if (response < 0)
-							detenido = true;
-					}
-				}
-			}
 		}
 	}
 
 	if (response < 0) {
-		std::string error = "Error llamando a " + funcion + " con código " + std::to_string(getLastError());
-		std::cerr << error << std::endl;
+		socketD = INVALID_SOCKET;
+		error("Error llamando a " + funcion + " con código " + std::to_string(getLastError()));
+	}
+
+	return response;
+}
+
+void Servidor::aceptarConexiones() {
+	int response = 0;
+
+	struct sockaddr_in clientAddress;
+	int clientAddressLength = sizeof(clientAddress);
+	char ip[INET_ADDRSTRLEN];
+
+	while (! detenido) {
+		SOCKET newSocketD = accept(socketD, (struct sockaddr*) &clientAddress,
+				(socklen_t*) &clientAddressLength);
+
+		if (newSocketD != INVALID_SOCKET) {
+			inet_ntop(AF_INET, &(clientAddress.sin_addr), ip, INET_ADDRSTRLEN);
+			sesiones.push_back(Sesion(newSocketD, ip));
+		} else if (!detenido)
+			response = -1;
+
+		if (response < 0)
+			detenido = true;
+	}
+
+	if (response < 0) {
+		error("Error llamando a accept con código " + std::to_string(getLastError()));
 	} else {
-		std::cout << "Finalizando servidor" << std::endl;
+		debug("Finalizando thread aceptarConexiones");
 	}
 }
 
 void Servidor::detener() {
 	if (!detenido) {
 		detenido = true;
-		closesocket(socketD);
 
-		std::cout << "Socket cerrado" << std::endl;
+		if (socketD != INVALID_SOCKET) {
+			closesocket(socketD);
+			debug("Socket cerrado");
 
-		auto it = sesiones.begin();
-		while (it != sesiones.end()) {
-			it->detener();
-			it++;
+
+			auto it = sesiones.begin();
+			while (it != sesiones.end()) {
+				it->detener();
+				it++;
+			}
+			sesiones.clear();
+
+			debug("Esperando que termine thread aceptarConexiones");
+			thread.join();
+			debug("Thread aceptarConexiones termino");
 		}
-		sesiones.clear();
-
-		std::cout << "Esperando que termine thread aceptarConexiones" << std::endl;
-
-		thread.join();
-
-		std::cout << "Thread aceptarConexiones termino" << std::endl;
+		info("Servidor detenido");
 	}
 }
 
--- tp1/src/Servidor.h
+++ tp1/src/Servidor.h
@@ -3,24 +3,24 @@
 
 #include "stdafx.h"
 #include "Sesion.h"
-#include "Mensajeria.h"
-#include "Usuarios.h"
 
 class Servidor {
 
 public:
-	Servidor(std::string &archivo);
+	Servidor(int puerto);
 	~Servidor();
 
-	void iniciar(int puerto);
+	void iniciar();
 	void detener();
 private:
 	SOCKET socketD;
+	int puerto;
 	bool detenido;
 	std::thread thread;
 	std::vector<Sesion> sesiones;
 
-	void aceptarConexiones(int port);
+	int crearSocket();
+	void aceptarConexiones();
 };
 
 #endif // SERVIDOR_H
